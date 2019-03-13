f84245856150caf8e7eceaff3e1d25be401ead39
Sara Stanton <sara.mpa13@gmail.com>
2019-03-13 01:18:15 -0500

    added js to update display output window when button clicked

--- calculator.js
+++ calculator.js
@@ -1,19 +1,40 @@
-const row1 = document.getElementsByClass("row-1")
-const row2 = document.getElementsByClass("row-2")
-const row3 = document.getElementsByClass("row-3")
-const row4 = document.getElementsByClass("row-4")
-const row5 = document.getElementsByClass("row-5")
+// Calculator function
+let displayVal = '0';
+let pendingVal;
+let evalStringArray = [];
+const btn = document.getElementsByClassName("calc-btn");
+
+updateDisplayVal = (clickObj) => {
+  let btnText = clickObj.target.innerText;
+
+  if(displayVal === '0') {
+    displayVal = '';
+  }
+
+  displayVal += btnText;
+  document.getElementById("display-output").innerText = displayVal
+}
+
+Array.from(btn).forEach(button => button.addEventListener('click', (e) => updateDisplayVal(e)) )
+
+
+// Color theme change
+
+const row1 = document.getElementsByClassName("row-1")
+const row2 = document.getElementsByClassName("row-2")
+const row3 = document.getElementsByClassName("row-3")
+const row4 = document.getElementsByClassName("row-4")
+const row5 = document.getElementsByClassName("row-5")
 
 const radio = document.getElementsByName("color-theme")
 
-radio.forEach(
-  element => element.addEventListener('click', replaceClassNames(event))
-)
-
-
-replaceClassNames(event) {
-  switch() {
-    case event.target.value === "":
-    row1.className = ""
-  }
-}
+// radio.forEach(
+//   element => element.addEventListener('click', replaceClassNames(event))
+// )
+//
+// replaceClassNames(event) {
+//   switch() {
+//     case event.target.value === "":
+//     row1.className = ""
+//   }
+// }
