37e4921e2b937f83ae6ebe568b7e28974d253ced
Sara Stanton <sara.mpa13@gmail.com>
2019-03-12 23:11:03 -0500

    added click event for radio buttons

--- calculator.js
+++ calculator.js
@@ -1,1 +1,19 @@
-const test = "test"
+const row1 = document.getElementsByClass("row-1")
+const row2 = document.getElementsByClass("row-2")
+const row3 = document.getElementsByClass("row-3")
+const row4 = document.getElementsByClass("row-4")
+const row5 = document.getElementsByClass("row-5")
+
+const radio = document.getElementsByName("color-theme")
+
+radio.forEach(
+  element => element.addEventListener('click', replaceClassNames(event))
+)
+
+
+replaceClassNames(event) {
+  switch() {
+    case event.target.value === "":
+    row1.className = ""
+  }
+}
