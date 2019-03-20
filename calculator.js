0b9944f11ccd5075169f5f56bb46d6c04cb0066e
Sara Stanton <sara.mpa13@gmail.com>
2019-03-20 00:52:27 -0500

    added operator logic

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+
 // CALCULATOR LOGIC
 let displayVal = '0';
 let pendingVal;
@@ -22,10 +23,26 @@ keydownFunction = (keyVal) => {
     switch(keyVal) {
       case 'Delete':
         clearDisplay();
+        break;
       case 'Backspace':
         backspace();
+        break;
       case '.':
         addDecimal();
+        break;
+      case '+':
+        performOperation("calc-add");
+        break;
+      case '-':
+        performOperation("calc-subtract");
+        break;
+      case '*':
+        performOperation("calc-multiply");
+        break;
+      case '/':
+        performOperation("calc-divide");
+        break;
+
     }
   }
 }
@@ -45,11 +62,10 @@ addDecimal = () => {
 
 flipSign = () => {
   let displayInt = parseInt(displayVal)*-1;
-  displayVal = displayInt.toString(); 
+  displayVal = displayInt.toString();
   updateDisplayWindow()
 }
 
-updateDisplayWindow()
 
 // OPERATOR FUNCTIONS
 clearDisplay = () => {
@@ -59,16 +75,43 @@ clearDisplay = () => {
   updateDisplayWindow()
 }
 
+performOperation = (clickObjId) => {
+  switch(clickObjId) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      console.log(clickObjId)
+      evalStringArray.push(displayVal);
+      evalStringArray.push(clickObjId)
+      displayVal = ' '
+      break;
+    case "calc-equals":
+      console.log(clickObjId)
+      evalStringArray.push(displayVal);
+      displayVal = eval(evalStringArray.join(' ')).toString();
+      updateDisplayWindow();
+      break;
+  }
+}
+
 
 
 // EVENT LISTENERS
 const numBtn = document.getElementsByClassName("num");
+const operators = document.getElementsByClassName("operator");
 const clrBtn = document.getElementById("calc-clear")
 const bksp = document.getElementById("calc-backspace")
 const decimal = document.getElementById("calc-decimal")
 const plusMinus = document.getElementById("calc-plusminus")
 
-Array.from(numBtn).forEach(button => button.addEventListener('click', (event) => updateDisplayVal(parseInt(event.target.innerText))) )
+Array.from(numBtn).forEach(button =>
+   button.addEventListener('click', (event) => updateDisplayVal(parseInt(event.target.innerText)))
+ )
+
+Array.from(operators).forEach(button =>
+   button.addEventListener('click', (event) => performOperation(event.target.id))
+ )
 
 document.addEventListener('keydown', (event) => keydownFunction(event.key) )
 
@@ -79,7 +122,6 @@ bksp.addEventListener('click', () => backspace())
 decimal.addEventListener('click', () => addDecimal())
 
 plusMinus.addEventListener('click', () => flipSign())
-
 
 // Color theme change
 
@@ -101,3 +143,6 @@ const radio = document.getElementsByName("color-theme")
 //     row1.className = ""
 //   }
 // }
+
+
+updateDisplayWindow();
